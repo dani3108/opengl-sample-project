3d76c196b1b2610124d8814325b081cddbf4d564
dani3108 <danielberrozpe4@gmail.com>
2026-05-07 21:18:32 +0200

    Added global vars for window size

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include <cstddef>
 #include <iostream>
 
+constexpr int DISPLAY_WIDTH { 800 };
+constexpr int DISPLAY_HEIGHT { 600 };
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 void processInput(GLFWwindow* window);
@@ -18,7 +21,7 @@ int main()
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     #endif
 
-    GLFWwindow* window { glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL) };
+    GLFWwindow* window { glfwCreateWindow(DISPLAY_WIDTH, DISPLAY_HEIGHT, "LearnOpenGL", NULL, NULL) };
     if(!window)
     {
         std::cerr << "Failed to create GLFW window\n";
